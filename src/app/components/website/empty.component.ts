2d534bd4bb324d90d67e1a5b7bf5c60a8154f958
Shaiful Islam <shaiful.islam@egeneration.co>
2022-02-21 23:14:03 +0600

    modification 2

---
+++ src/app/components/website/empty.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {WebsiteService} from './website.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+
+@Component({
+    selector: 'app-website',
+    template: ``
+})
+export class EmptyComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+
+    }
+
+}
--- src/app/components/website/website.module.ts
+++ src/app/components/website/website.module.ts
@@ -1,41 +1,68 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {Injectable, NgModule} from '@angular/core';
+import {RouterModule, ROUTES, Routes} from '@angular/router';
 import {WebsiteComponent} from './website.component';
-import {RouterModule} from '@angular/router';
-import {HeaderOneModule} from '../../shared/module/header-one/header-one.module';
 import {AuthGuard} from '../../shared/services/auth/auth.guard';
+import {CommonModule} from '@angular/common';
+import {HeaderOneModule} from '../../shared/module/header-one/header-one.module';
 import {WebsiteService} from './website.service';
 
+@Injectable()
+export class RouteDecideService{
+
+  constructor() {}
+
+  getSeletedThemeForOrg(): string {
+    localStorage.setItem('selectedTheme', 'padma');
+    return localStorage.getItem('selectedTheme');
+    //return 'padma';
+  }
+}
+
+export function getRoutes(rds: RouteDecideService) {
+  let routes: Routes = [];
+  if (rds.getSeletedThemeForOrg() === 'padma'){
+    routes = [ { path: '', component: WebsiteComponent, children: [
+          {path: '', loadChildren: () => import('./website-home-padma/website-home-padma.module')
+                .then(m => m.WebsiteHomePadmaModule) , canActivate: [AuthGuard] }
+        ]}
+    ];
+  }
+  if (rds.getSeletedThemeForOrg() === 'jamuna'){
+    routes = [ { path: '', component: WebsiteComponent, children: [
+        {path: ':orgName/jamuna', loadChildren: () => import('./website-home-jamuna/website-home-jamuna.module')
+              .then(m => m.WebsiteHomeJamunaModule) , canActivate: [AuthGuard] },
+      ]}
+    ];
+  }
+  return routes;
+}
+
 @NgModule({
-  imports: [RouterModule.forChild([
-    { path: '', component: WebsiteComponent,
-      children: [
-        {path: '', loadChildren: () => import('./website-home-padma/website-home-padma.module')
-              .then(m => m.WebsiteHomePadmaModule) , canActivate: [AuthGuard] },
-        /*{path: 'cart', loadChildren: () => import('./website-home-padma/cart/cart.module').then(m => m.CartModule) , canActivate: [AuthGuard] },
-        {path: 'checkout', loadChildren: () => import('./website-home-padma/checkout/checkout.module').then(m => m.CheckoutModule) , canActivate: [AuthGuard] },
-        {path: 'category/:category', loadChildren: () => import('./website-home-padma/category/category.module').then(m => m.CategoryModule) , canActivate: [AuthGuard] },
-        {path: 'product/:product', loadChildren: () => import('./website-home-padma/product/product.module').then(m => m.ProductModule) , canActivate: [AuthGuard] },
-        {path: ':dynamicPage', loadChildren: () => import('./website-home-padma/dynamic-page/dynamic-page.module').then(m => m.DynamicPageModule) , canActivate: [AuthGuard] },
-        */{path: '**', redirectTo: 'home', },
-      ]
-    }
-  ])],
-  exports: [RouterModule],
-  declarations: []
+  declarations: [],
+  imports: [RouterModule.forRoot([])],
+  providers: [
+    {
+      provide: ROUTES,
+      useFactory: getRoutes,
+      deps: [RouteDecideService],
+      multi: true,
+    },
+    RouteDecideService,
+  ]
 })
-export class  WebsiteRoutingModule{}
+export class RouterDecideModule { }
 
 @NgModule({
   declarations: [WebsiteComponent],
   imports: [
     CommonModule,
-    WebsiteRoutingModule,
-    // RouterDecideModule,
+    RouterModule.forChild([]),
+    // WebsiteRoutingModule,
+    RouterDecideModule,
     HeaderOneModule,
   ],
   providers: [WebsiteService]
 })
-export class WebsiteModule{}
+export class WebsiteModule {}
 
 
